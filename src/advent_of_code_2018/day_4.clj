7a2102b44a5b35657c322b90fe3072e9b86dc1f4
yijiahe <yi.jiahe00@gmail.com>
2023-07-13 14:02:25 +0000

    Refactor day 4

--- src/advent_of_code_2018/day_4.clj
+++ src/advent_of_code_2018/day_4.clj
@@ -6,50 +6,56 @@
   (clojure.edn/read-string (clojure.string/replace datetime #"[- :]" "")))
 
 (defn parse-record
+  "Splits a record (line of the input) into a list containing the date and message component"
   [record]
   (subvec (re-matches #"\[(.*)\] (.*)" record) 1))
 
 (defn sort-records-by-date
+  "Sorts the records by their date component"
   [records]
   (sort-by
    (fn [record]
      (parse-date (first (parse-record record))))
    records))
 
-(defn parse-message
-  [{:keys [sleeps current-sleep] :as acc} record]
-  (let [[datetime message] (parse-record record)]
-    (cond
-      (clojure.string/starts-with? message "Guard") (let [guard (nth (first (re-seq #"#([0-9]+)" message)) 1)]
-                                                        (assoc acc
-                                                               :current-sleep
-                                                               {:guard guard :start nil}))
-      (= message "falls asleep") (assoc acc
-                                        :current-sleep
-                                        (assoc current-sleep :start datetime))
-      (= message "wakes up") (assoc acc
-                                    :sleeps
-                                    (assoc sleeps 
-                                           (current-sleep :guard) 
-                                           (conj (sleeps (current-sleep :guard) []) [(current-sleep :start) datetime]))))))
+(defn compile-sleeps
+  "Parses the sorted records into a list of sleeps in the form [guard start end]"
+  [sorted-records]
+  ((reduce
+    (fn [{:keys [sleeps current-sleep] :as acc} record]
+      (let [[datetime message] (parse-record record)]
+        (cond
+          (clojure.string/starts-with? message "Guard") (let [guard (nth (first (re-seq #"#([0-9]+)" message)) 1)]
+                                                          (assoc acc
+                                                                 :current-sleep
+                                                                 {:guard guard :start nil}))
+          (= message "falls asleep") (assoc acc
+                                            :current-sleep
+                                            (assoc current-sleep :start datetime))
+          (= message "wakes up") (assoc acc
+                                        :sleeps
+                                        (assoc sleeps
+                                               (current-sleep :guard)
+                                               (conj (sleeps (current-sleep :guard) []) [(current-sleep :start) datetime]))))))
+    {:sleeps {} :current-sleep {:guard nil :start nil}}
+    sorted-records)
+   :sleeps))
 
-(defn total-minutes-asleep
+(defn compile-time-slept-by-guards
+  "Returns a map of guards and the total time spent sleeping across all days from the list of sleeps"
   [sleeps]
-  (reduce (fn [time-sleeping [guard sleep-times]]
-            (assoc time-sleeping guard (reduce
-                                        (fn [time-asleep [start end]]
-                                          (+ time-asleep (reduce - (map parse-date [end start]))))
-                                        0
-                                        sleep-times)))
-          {}
-          sleeps))
+  (reduce
+   (fn [time-sleeping [guard sleep-times]]
+     (assoc time-sleeping guard (reduce
+                                 (fn [time-asleep [start end]]
+                                   (+ time-asleep (reduce - (map parse-date [end start]))))
+                                 0
+                                 sleep-times)))
+   {}
+   sleeps))
 
 (defn part-1
   [sorted-records]
-  (let [sleeps ((reduce
-                 parse-message
-                 {:sleeps {} :current-sleep {:guard nil :start nil}}
-                 sorted-records)
-                :sleeps)]
-    (let [sleepiest-guard (key (apply max-key val (total-minutes-asleep sleeps)))]
-      sleepiest-guard)))
+  (let [sleeps (compile-sleeps sorted-records)
+        sleepiest-guard (key (apply max-key val (compile-time-slept-by-guards sleeps)))]
+    sleepiest-guard))
