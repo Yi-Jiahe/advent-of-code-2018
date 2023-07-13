415773001474ca950dcdcfcc64282ac1dc40a990
yijiahe <yi.jiahe00@gmail.com>
2023-07-13 13:05:12 +0000

    Add assertion for day 4 part 1 test

--- test/advent_of_code_2018/day_4_test.clj
+++ test/advent_of_code_2018/day_4_test.clj
@@ -37,5 +37,4 @@
 (deftest end-to-end-test
   (testing "Example 1"
     (let [sorted-records (sort-records-by-date (parse-input example-records))]
-      ;; (run! println sorted-records)
-      (println (part-1 sorted-records)))))
+      (is (= (part-1 sorted-records) 240)))))
